0e8c96fc80eaa2950991b91759844fd15105e40e
rahowa <roninfromsvf@gmail.com>
2023-04-14 18:42:54 +0300

    Update example

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
+import numpy as np
 import gnss_localization as gnssl
 
-data = [[1621693264.0155628, 9521, -35074, 3.92, -1.35],
-        [1621693264.1979840, 9450, -34970, 3.93, -1.22],
-        [1621693264.4237902, 9365, -34853, 3.85, -1.24],
-        [1621693264.6384845, 9291, -34759, 3.85, -1.12]]
+gnss_coordinates = [
+    [1621693264.0155628, 9521, -35074, 3.92, -1.35],
+    [1621693264.1979840, 9450, -34970, 3.93, -1.22],
+    [1621693264.4237902, 9365, -34853, 3.85, -1.24],
+    [1621693264.6384845, 9291, -34759, 3.85, -1.12],
+]
 
 estimator = gnssl.GNSSProjectionEstimator(
     offset_x=0.0,
@@ -12,22 +15,25 @@ estimator = gnssl.GNSSProjectionEstimator(
 )
 
 projection = [
-    estimator.predict([x, y, 0, 0, pitch, roll])
-    for (_, x, y, roll, pitch) in data
+    estimator.predict([x, y, 0, pitch, roll, 0])
+    for (_, x, y, roll, pitch) in gnss_coordinates
 ]
+print(projection)  # [[], [] ... ]
 
-print(projection) # [[], [] ... ]
 
-estimator = gnssl.GNSSHeadingEstimator(
+coord_estimator = gnssl.GNSSVehicleCoordinatesEstimator(
     offset_x=0.0,
     offset_y=0.0,
     offset_z=1500.0,
-    north_direction=(0,1,0)
 )
-
-heading = [
-    estimator.predict([x, y, 0, 0, pitch, roll])
-    for (_, x, y, roll, pitch) in data
+vehicle_coordinates = [
+    coord_estimator.predict([x, y, 0, pitch, roll, 0])
+    for (_, x, y, roll, pitch) in gnss_coordinates
 ]
+print(vehicle_coordinates)
 
-print(heading) # [163, 166, ... ]
+estimator = gnssl.GNSSHeadingEstimator(
+    offset_x=0.0, offset_y=0.0, offset_z=1500.0, north_direction=(0, 1, 0)
+)
+heading = estimator.predict(np.asanyarray(vehicle_coordinates).reshape(-1, 3))
+print(heading) [43.02760911 55.47551315 50.60675111]
